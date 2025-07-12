f6df015ae97bf58217c9d5edae1385d9bc08aa87
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-12 05:02:38 -0400

    first player instance

--- main.py
+++ main.py
@@ -2,24 +2,28 @@ from constants import *
 
 import pygame
 
-def main():
-    pygame.init()
-    fps = pygame.time.Clock()
-    dt = 0
-    fps_limit = 60
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+from player import Player
 
-    while True:
-        pygame.display.set_caption(f"Asteroids - Frame: {fps}")
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        
-        screen.fill("black")
-        
-        dt = fps.tick(fps_limit) / 1000
-        pygame.display.flip()
+def main():
+  pygame.init()
+  fps = pygame.time.Clock()
+  dt = 0
+  fps_limit = 60
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+  while True:
+    pygame.display.set_caption(f"Asteroids - Frame: {fps}")
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        return
+
+    screen.fill("black")
+    player.draw(screen)
+
+    dt = fps.tick(fps_limit) / 1000
+    pygame.display.flip()
 
 
 if __name__ == "__main__":
-    main()
+  main()
