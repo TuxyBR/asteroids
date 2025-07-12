ef1d10f12bcf27632f283644bf7cd93077354aa5
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-12 08:26:54 -0400

    jagged random asteroids!

--- asteroid.py
+++ asteroid.py
@@ -1,34 +1,56 @@
 import pygame
 import random
 from circleshape import CircleShape
-from constants import ASTEROID_MIN_RADIUS, PLAYER_RADIUS
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
     super().__init__(x, y, radius)
+    
     self.rotation = 0
-    self.move_direction = 0
+    self.rotation_speed = random.randrange(-15, 15)
+    
+    self.sides = random.randrange(8, 20)
+    self.jaggedness = random.uniform(0.3, 0.5)
+    self.points = self.rand_polygon()
   
-  def draw(self, screen): #TODO: sprite based system
-    pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+  def rand_polygon(self):
+    points = []
+    angle_step = 360 / self.sides
+    for i in range(self.sides):
+      angle = i * angle_step
+      direction = pygame.Vector2(0, -1).rotate(angle)
+      point = direction * self.radius * random.uniform(1 - self.jaggedness, 1 + self.jaggedness)
+      points.append(point)
+    return points
+  
+  def draw(self, screen):
+    points = []
+    for point in self.points:
+      rotated = point.rotate(self.rotation)
+      points.append(self.position + rotated)
+    pygame.draw.polygon(screen, "white", points, 1)
+
     
   def update(self, dt):
+    self.rotate(dt)
     self.move(dt)
     
   def move(self, dt):
     self.position += self.velocity * dt
     
   def rotate(self, dt):
-    pass
+    self.rotation += (self.rotation_speed * dt) % 360
   
   def split(self):
     self.kill()
     angle = random.uniform(20, 50)
     if self.radius > ASTEROID_MIN_RADIUS:
-      for i in range(random.randrange(1, 5)):
-        angle *= random.uniform(1, 2)
+      asteroid_amount = random.randrange(2, 4)
+      for _ in range(asteroid_amount):
         new_vector = self.velocity.rotate(angle)
-        new_radius = self.radius - random.uniform(15, self.radius-25)
-        new_asteroid = Asteroid(self.position[0], self.position[1], new_radius)
-        new_asteroid.velocity = new_vector * random.uniform(1.2, 1.5)
+        angle *= random.uniform(1, 2)
+        new_radius = self.radius - random.uniform(10*asteroid_amount, (self.radius-15))
+        new_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid.velocity = new_vector * random.uniform(0.7, 1.4)
         
