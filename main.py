5683bfc4c11e384d9486fc345d450097e1b5927e
TuxyBR <rafinhalm.lm@gmail.com>
2025-07-12 04:28:53 -0400

    initial game setup

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from constants import *
 
+import pygame
+
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    fps = pygame.time.Clock()
+    dt = 0
+    fps_limit = 60
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        pygame.display.set_caption(f"Asteroids - Frame: {fps}")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+        
+        dt = fps.tick(fps_limit) / 1000
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
