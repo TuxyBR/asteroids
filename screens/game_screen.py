a570e633f0a65b1d0fb5fc086e99b6b808f92e8b
TuxyBR <rafinhalm.lm@gmail.com>
2025-09-29 06:05:48 -0400

    handles for MP respawn, pauses etc

--- screens/game_screen.py
+++ screens/game_screen.py
@@ -1,7 +1,10 @@
+import random
+
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from entities.player import Player
+from entities.player_input import PlayerInputState
 from entities.asteroid import Asteroid
 from entities.asteroid_field import AsteroidField
 from entities.shot import Shot
@@ -22,7 +25,15 @@ class GameScreen:
     Shot.containers = (self.shot_group, self.update_group, self.draw_group)
     Explosion.containers = (self.update_group, self.draw_group)
 
+    self.players = []
+    self.player_registry = {}
+    self.player_inputs = {}
+    self.pending_respawns = {}
+    self.allow_respawns = False
+    self.respawn_delay = 3.0
+
     self.player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    self._register_player(self.player)
     self.asteroid_field = asteroid_field
     if self.asteroid_field is None:
       self.asteroid_field = AsteroidField(self.asteroid_group, self.update_group)
@@ -30,12 +41,12 @@ class GameScreen:
       self.asteroid_field.asteroid_group = self.asteroid_group
       self.asteroid_field.add(self.update_group)
 
-    self.players = [self.player]
     self.explosions = []
     self.score = 0
 
     self.hud_font = pygame.font.SysFont("monospace", 36, bold=True)
     self.pause_menu = None
+    self.uses_local_keyboard = True
 
   def _set_all_players_paused(self, paused):
     for player in self.players:
@@ -43,6 +54,44 @@ class GameScreen:
 
   def _all_players_paused(self):
     return all(player.is_paused() for player in self.players)
+
+  def _register_player(self, player: Player):
+    self.players.append(player)
+    self.player_registry[player.player_id] = player
+    self.player_inputs.setdefault(player.player_id, player.input_state)
+    return player
+
+  def spawn_player(self, x=None, y=None, player_id=None):
+    if x is None:
+      x = random.uniform(SCREEN_WIDTH * 0.25, SCREEN_WIDTH * 0.75)
+    if y is None:
+      y = random.uniform(SCREEN_HEIGHT * 0.25, SCREEN_HEIGHT * 0.75)
+
+    player = Player(x, y, player_id=player_id)
+    return self._register_player(player)
+
+  def get_player(self, player_id):
+    return self.player_registry.get(player_id)
+
+  def set_player_input(self, player_id, input_state):
+    self.player_inputs[player_id] = input_state
+    player = self.player_registry.get(player_id)
+    if player is not None:
+      player.set_input_state(input_state)
+      return
+
+    respawn_info = self.pending_respawns.get(player_id)
+    if respawn_info and respawn_info.get("awaiting_input"):
+      if self._input_requests_respawn(input_state):
+        self._respawn_player(player_id)
+
+  def remove_player(self, player_id):
+    player = self.player_registry.pop(player_id, None)
+    if player is None:
+      return
+    if player in self.players:
+      self.players.remove(player)
+    player.kill()
 
   def handle_event(self, event):
     if self.pause_menu is not None:
@@ -64,9 +113,8 @@ class GameScreen:
       return None
 
     if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-      # For now, treat the primary player as the one requesting a pause.
       if self.players:
-        self.players[0].set_paused(True)
+        self._set_all_players_paused(True)
       if self._all_players_paused() and self.pause_menu is None:
         self.pause_menu = PauseMenu()
 
@@ -79,11 +127,20 @@ class GameScreen:
 
     pygame.display.set_caption(f"Asteroids by TuxyBR - Score: {self.score}")
 
+    if self.uses_local_keyboard and self.players:
+      primary_player = self.players[0]
+      primary_state = PlayerInputState.from_keyboard()
+      self.set_player_input(primary_player.player_id, primary_state)
+
+    self._update_respawns(dt)
     self.update_group.update(dt)
 
     for asteroid in list(self.asteroid_group):
-      for player in self.players:
+      for player in list(self.players):
         if asteroid.collides_with(player):
+          if self.allow_respawns:
+            self._handle_player_death(player)
+            break
           return ("game_over", {"score": self.score})
 
       for shot in list(self.shot_group):
@@ -111,3 +168,54 @@ class GameScreen:
       overlay.fill((0, 0, 0, 180))
       surface.blit(overlay, (0, 0))
       self.pause_menu.draw(surface)
+
+  def _handle_player_death(self, player: Player):
+    player_id = player.player_id
+    self.remove_player(player_id)
+    self.pending_respawns[player_id] = {
+      "time_left": self.respawn_delay,
+      "awaiting_input": False,
+    }
+
+  def _update_respawns(self, dt):
+    to_respawn = []
+    for player_id, info in list(self.pending_respawns.items()):
+      if not info["awaiting_input"]:
+        info["time_left"] -= dt
+        if info["time_left"] <= 0:
+          info["time_left"] = 0
+          info["awaiting_input"] = True
+
+      if info["awaiting_input"]:
+        input_state = self.player_inputs.get(player_id)
+        if input_state and self._input_requests_respawn(input_state):
+          to_respawn.append(player_id)
+
+    for player_id in to_respawn:
+      self._respawn_player(player_id)
+
+  def _respawn_player(self, player_id):
+    input_state = self.player_inputs.get(player_id, PlayerInputState())
+    player = self.spawn_player(player_id=player_id)
+    player.set_input_state(input_state)
+    self.player_inputs[player_id] = input_state
+    self.pending_respawns.pop(player_id, None)
+
+  def _input_requests_respawn(self, input_state: PlayerInputState) -> bool:
+    return bool(
+      input_state.shoot
+      or input_state.thrust
+      or input_state.reverse
+      or input_state.rotate_left
+      or input_state.rotate_right
+    )
+
+  def get_respawn_status(self):
+    status = []
+    for player_id, info in self.pending_respawns.items():
+      if info["awaiting_input"]:
+        message = "Press any control to respawn"
+      else:
+        message = f"Respawn in {info['time_left']:.1f}s"
+      status.append((player_id, message))
+    return status
