f762d61b4bd23fe7b7fc62d3f5cc7ded3ae1e7f2
TuxyBR <rafinhalm.lm@gmail.com>
2025-09-29 03:15:45 -0400

    screen wrap prep

--- entities/circle_shape.py
+++ entities/circle_shape.py
@@ -1,5 +1,7 @@
 import pygame
 
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 class FormaCirculo(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
         # we will be using this later
@@ -22,3 +24,23 @@ class FormaCirculo(pygame.sprite.Sprite):
     
     def collides_with(self, other):
         return self.position.distance_to(other.position) < (self.radius + other.radius)
+
+    def wrap_position(self):
+        wrapped_x = False
+        wrapped_y = False
+
+        if self.position.x < -self.radius:
+            self.position.x = SCREEN_WIDTH + self.radius
+            wrapped_x = True
+        elif self.position.x > SCREEN_WIDTH + self.radius:
+            self.position.x = -self.radius
+            wrapped_x = True
+
+        if self.position.y < -self.radius:
+            self.position.y = SCREEN_HEIGHT + self.radius
+            wrapped_y = True
+        elif self.position.y > SCREEN_HEIGHT + self.radius:
+            self.position.y = -self.radius
+            wrapped_y = True
+
+        return wrapped_x, wrapped_y
