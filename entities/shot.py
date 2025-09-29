a5c69f351f7d918c22d84e26af7b149b95e536a5
TuxyBR <rafinhalm.lm@gmail.com>
2025-09-29 06:04:40 -0400

    unique shots (for mp)

--- entities/shot.py
+++ entities/shot.py
@@ -1,10 +1,13 @@
+import uuid
+
 import pygame
 from .circle_shape import CircleShape
 from constants import SHOT_RADIUS
 
 class Shot(CircleShape):
-  def __init__(self, x, y):
+  def __init__(self, x, y, entity_id=None):
     super().__init__(x, y, SHOT_RADIUS)
+    self.entity_id = entity_id or uuid.uuid4().hex
   
   def draw(self, screen):
     pygame.draw.circle(screen, "white", self.position, self.radius, 2)
